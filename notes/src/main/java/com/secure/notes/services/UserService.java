ea90bbf1b5b7fd8d743467a42c8f028004f0300d
245ansh <245anshtamrakar@gmail.com>
2025-04-17 16:48:11 +0530

    login signup through jwt implemented

--- notes/src/main/java/com/secure/notes/services/UserService.java
+++ notes/src/main/java/com/secure/notes/services/UserService.java
@@ -8,6 +8,10 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import java.util.List;
 public interface UserService {
     void updateUserRole(Long userId, String roleName);
+
     List<User> getAllUsers();
+
     UserDTO getUserById(Long id);
+
+    User findByUsername(String username);
 }
